0572b874165f21479a88a7494580ec35e448c94c
Wei Li <limijd@gmail.com>
2013-05-13 00:02:04 -0700

    add script to run arduino data server forever

---
+++ run_arduino_data_server_always.sh
@@ -0,0 +1,10 @@
+#!/bin/csh
+
+ps -A |grep arduino_app_ >& /dev/null
+
+if $? then
+    echo "nohup /home/limijd/local.install/github.scripts/arduino/arduino_app_layer.py --port=21599 --serial_port=/dev/ttyACM0 >& /dev/null"
+    nohup  /home/limijd/local.install/github.scripts/arduino/arduino_app_layer.py --port=21599 --serial_port=/dev/ttyACM0 >& /dev/null
+else
+    exit 0
+endif
