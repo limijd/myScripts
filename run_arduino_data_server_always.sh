71410078dd12fe1566b20690220f09db57660280
Wei Li <limijd@gmail.com>
2013-05-13 00:06:07 -0700

    fix

--- run_arduino_data_server_always.sh
+++ run_arduino_data_server_always.sh
@@ -4,7 +4,7 @@ ps -A |grep arduino_app_ >& /dev/null
 
 if $? then
     echo "nohup /home/limijd/local.install/github.scripts/arduino/arduino_app_layer.py --port=21599 --serial_port=/dev/ttyACM0 >& /dev/null"
-    nohup  /home/limijd/local.install/github.scripts/arduino/arduino_app_layer.py --port=21599 --serial_port=/dev/ttyACM0 >& /dev/null
+    nohup  /home/limijd/local.install/github.scripts/arduino/arduino_app_layer.py --port=21599 --serial_port=/dev/ttyACM0 >& /dev/null &
 else
     exit 0
 endif
