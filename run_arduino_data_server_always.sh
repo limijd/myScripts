42b33c91115dbfdf25d761efbfe457068926c5a6
Wei Li <limijd@gmail.com>
2013-09-17 00:19:14 -0700

    run daemon for arduino

--- run_arduino_data_server_always.sh
+++ run_arduino_data_server_always.sh
@@ -3,8 +3,8 @@
 ps -A |grep arduino_app_ >& /dev/null
 
 if $? then
-    echo "nohup /home/limijd/local.install/github.scripts/arduino/arduino_app_layer.py --port=21599 --serial_port=/dev/ttyACM0 >& /dev/null"
-    nohup  /home/limijd/local.install/github.scripts/arduino/arduino_app_layer.py --port=21599 --serial_port=/dev/ttyACM0 >& /dev/null &
+    echo "nohup sudo /home/limijd/local.install/github.scripts/arduino/arduino_app_layer.py --port=21599 --serial_port=/dev/ttyACM0 >& /dev/null"
+    nohup  sudo /home/limijd/local.install/github.scripts/arduino/arduino_app_layer.py --port=21599 --serial_port=/dev/ttyACM0 >& /dev/null &
 else
     exit 0
 endif
